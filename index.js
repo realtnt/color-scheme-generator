6d2813fc58bdda0546ae4816efcd678a3e3d7583
Theo <theo@thetaphoto.co.uk>
2022-03-27 02:21:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const colorModeSelect = document.getElementById("color-mode")
+const colorSchemeBtn = document.getElementById("color-btn")
+const colorPicker = document.getElementById("color-picker")
+const mainEl = document.querySelector("main")
+const footerEl = document.querySelector("footer")
+const snackbar = document.getElementById("snackbar")
+const baseUrl = "https://www.thecolorapi.com/scheme"
+
+randomColor = Math.floor(Math.random() * 16777215)
+
+colorPicker.value = '#' + randomColor.toString(16)
+getColors()
+console.log(colorPicker.value)
+
+colorSchemeBtn.addEventListener("click", () => {
+    getColors()
+})
+
+function getColors() {
+    fetch(`${baseUrl}?hex=${colorPicker.value.slice(1)}&${colorModeSelect.value}`)
+        .then(res => res.json())
+        .then(data => renderColors(data.colors))
+}
+
+function renderColors(colorsArray) {
+    mainEl.innerHTML = colorsArray.map(color => {
+        return `
+            <div 
+                onclick="copyToClipboard('${color.hex.value}')"
+                class="colors" 
+                style="background-color: ${color.hex.value}">
+                <div class="color-name">${color.name.value}</div>
+            </div>
+    `
+    }).join('')
+
+    footerEl.innerHTML = colorsArray.map(color => {
+        return `
+            <p 
+                onclick="copyToClipboardAsync('${color.hex.value}')"
+                class="color-hex">
+                ${color.hex.value}
+            </p>
+        `
+    }).join('')
+}
+
+function copyToClipboard(str) {
+    copyToClipboardAsync(str)
+    snackbar.className = "show"
+    setTimeout(() => snackbar.className = "", 2000)
+}
+
+function copyToClipboardAsync(str) {
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+        return navigator.clipboard.writeText(str)
+    return Promise.reject('The Clipboard API is not available.')
+}
